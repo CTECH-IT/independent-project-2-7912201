0ec4bba1a4fc21686fadf5060e58ca2e16898cac
Caleb Pardeik <7912201@isd535.org>
2022-03-01 11:33:00 -0600

    AD sidebar style AD article text ST java onclick

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
-// Put your JavaScript here
+const NEWTEXT = '[data-role="newtext"]';
+const ARTICLETITLE = '[data-role="articletitle"]';
+const ARTICLETEXT = '[data-role="articletext"]';
+
+console.log(NEWTEXT.getAttribute("data-title"));
+console.log(ARTICLETEXT);
+console.log(ARTICLETITLE);
+
+function changeArticle(title, text) {
+    document
+}
+
+//get element by id, change the text and images
