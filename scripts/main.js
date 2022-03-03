953098dba57b8dbb8b69f7b17e2a45e3ec2ebad9
Caleb Pardeik <7912201@isd535.org>
2022-03-03 11:15:48 -0600

    AD rickroll AD favicon

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,27 @@
 //change the articletitle and articletext to title and text provided by the element clicked.
 //happens onclick
 
-function changeArticle(title, text) {
+function changeArticle(title, text, link) {
     document.getElementById("articletitle").innerHTML = title;
     document.getElementById("articletext").innerHTML = text;
+    document.getElementById("donation").innerHTML = link;
     document.getElementById("article").classList.add("articleanimation");
     setTimeout(function() {
         document.getElementById("article").classList.remove("articleanimation");
     }, 200)
 }
+
+document.body.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 32) {
+        document.getElementById("rickroll").classList.add("rickroll");
+    }
+})
+
+document.body.addEventListener("keydown", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 32) {
+        document.getElementById("rickroll").classList.remove("rickroll");
+    }
+})
+
