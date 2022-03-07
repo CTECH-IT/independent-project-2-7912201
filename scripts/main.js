5df61249d8b5fa2a2fa1f74dc392f7a37bea0450
Caleb Pardeik <7912201@isd535.org>
2022-03-06 20:51:03 -0600

    AD articles AD images AD mobile view

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,13 @@
 //change the articletitle and articletext to title and text provided by the element clicked.
 //happens onclick
 
-function changeArticle(title, text, link, extratext) {
+function changeArticle(title, text, link, extratext, image1, image2) {
     document.getElementById("articletitle").innerHTML = title;
     document.getElementById("articletext").innerHTML = text;
     document.getElementById("donation").innerHTML = link;
     document.getElementById("extratext").innerHTML = extratext;
+    document.getElementById("image1").src = image1;
+    document.getElementById("image2").src = image2;
     document.getElementById("article").classList.add("articleanimation");
     setTimeout(function() {
         document.getElementById("article").classList.remove("articleanimation");
@@ -27,3 +29,7 @@ document.body.addEventListener("keydown", function(event) {
     }
 })
 
+let rickmyman = document.querySelector("#rickroll");
+let target = rickmyman.getAttribute("target");
+console.log(target);
+rickmyman.setAttribute("target", "_NothingToSeeHere");
